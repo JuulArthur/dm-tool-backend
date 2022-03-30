246e817a2827712783e55fcabaa8a2c0e86cd74b
Juul Arthur <juularthur92@gmail.com>
2022-03-30 15:13:14 +0200

    Make it possible to update order for chapter

--- server/chapter/chapter.ts
+++ server/chapter/chapter.ts
@@ -1,12 +1,40 @@
 import express from 'express';
-import { dbExecuteAsyncQuery } from '../db';
+import _ from 'lodash';
+import { dbExecuteAsyncQuery, dbUpdateRecord } from '../db';
 
 const router = express.Router();
 
+interface ChapterInterface {
+    id: string;
+    title?: string;
+    description?: string;
+    character_order?: number[];
+    location_order?: number[];
+}
+
+const mapChapterToDB = (chapter: any) => {
+    const mappedObject: ChapterInterface = _.pick(chapter, ['id', 'title', 'description'])
+    if (chapter.characterOrder) {
+        mappedObject['character_order'] = chapter.characterOrder;
+    }
+    if (chapter.locationOrder) {
+        mappedObject['location_order'] = chapter.locationOrder;
+    }
+    return mappedObject;
+}
+
+const mapChapterToFrontend = (chapter: any) => ({
+    id: chapter.id,
+    title: chapter.title,
+    description: chapter.description,
+    characterOrder: chapter.character_order,
+    locationOrder: chapter.location_order,
+})
+
 router.get('/all', async (req, res, next) => {
     try {
-        const characters = await dbExecuteAsyncQuery('SELECT * FROM chapter');
-        return res.status(200).json(characters);
+        const chapters = await dbExecuteAsyncQuery('SELECT * FROM chapter');
+        return res.status(200).json(chapters.map(mapChapterToFrontend));
     } catch (e) {
         return res.status(500).json(e);
     }
@@ -15,9 +43,20 @@ router.get('/all', async (req, res, next) => {
 router.get('/:id', async (req, res, next) => {
     try {
         const id = req.params.id;
-        const characters = await dbExecuteAsyncQuery('SELECT * FROM chapter WHERE id = $1', [id]);
-        return res.status(200).json(characters[0]);
+        const characterResult = await dbExecuteAsyncQuery('SELECT * FROM chapter WHERE id = $1', [id]);
+        return res.status(200).json(mapChapterToFrontend(characterResult[0]));
     } catch (e) {
+        return res.status(500).json(e);
+    }
+})
+
+router.patch('/:id', async (req, res, next) => {
+    try {
+        const chapterUpdates = {id: req.params.id, ...req.body};
+        const chapterResult = await dbUpdateRecord('chapter', mapChapterToDB(chapterUpdates));
+        return res.status(200).json(mapChapterToFrontend(chapterResult));
+    } catch (e) {
+        console.log('Failed to update chapter', e);
         return res.status(500).json(e);
     }
 })
