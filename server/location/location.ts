da9c00a69bdd5d926824c8026f2404c8f6b78652
Juul Arthur <juularthur92@gmail.com>
2022-08-17 22:58:16 +0200

    Make it possible to create new locations

--- server/location/location.ts
+++ server/location/location.ts
@@ -3,10 +3,45 @@ import { dbCreateRecord, dbExecuteAsyncQuery } from '../db';
 
 const router = express.Router();
 
+const mapLocationToBackend = (location: any) => ({
+    id: location.id,
+    name: location.name,
+    description: location.description,
+    info: location.info,
+    original_content: location.originalContent,
+});
+
+const mapLocationToFrontend = (location: any) => ({
+    id: location.id,
+    name: location.name,
+    description: location.description,
+    info: location.info,
+    originalContent: location.original_content,
+});
+
+const createLocationToChapterForLocation = async ({
+    chapterId,
+    locationId,
+}: {
+    chapterId: string,
+    locationId: string,
+}) => {
+    const locationWithHighestOrder = await dbExecuteAsyncQuery(
+        'SELECT * FROM location_to_chapter WHERE chapter_id = $1 ORDER BY order_id DESC LIMIT 1',
+        [chapterId]
+    );
+    const nextOrderNumber = locationWithHighestOrder[0] ? locationWithHighestOrder[0].order_id + 1 : 0;
+    return await dbCreateRecord('location_to_chapter', {
+        location_id: locationId,
+        chapter_id: chapterId,
+        order_id: nextOrderNumber,
+    });
+};
+
 router.get('/all', async (req, res, next) => {
     try {
         const locations = await dbExecuteAsyncQuery('SELECT * FROM location');
-        return res.status(200).json(locations);
+        return res.status(200).json(locations.map(mapLocationToFrontend));
     } catch (e) {
         return res.status(500).json(e);
     }
@@ -15,7 +50,7 @@ router.get('/all', async (req, res, next) => {
 router.get('/:id', async (req, res, next) => {
     try {
         const location = await dbExecuteAsyncQuery('SELECT * FROM location WHERE id = $1', [req.params.id]);
-        return res.status(200).json(location[0]);
+        return res.status(200).json(mapLocationToFrontend(location[0]));
     } catch (e) {
         return res.status(500).json(e);
     }
@@ -24,7 +59,12 @@ router.get('/:id', async (req, res, next) => {
 router.post('/', async (req: any, res: any) => {
     try {
         const data = req.body;
-        const result = await dbCreateRecord('location', data);
+        const mappedData = mapLocationToBackend(data);
+        delete mappedData.id;
+        const result = await dbCreateRecord('location', mappedData);
+        if (data.chapterId) {
+            await createLocationToChapterForLocation({ chapterId: data.chapterId, locationId: result.id });
+        }
         return res.status(200).json(result);
     } catch (e) {
         console.log('e', e);
